0ad3b61fe20215f755e6fddc3ea088054ecf1d9f
Cy <tennnnnnnnnn@live.com>
2013-10-16 09:36:09 +0800

    Initial Commit

---
+++ MyBigFace/MyBigFaceAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MyBigFaceAppDelegate.h
+//  MyBigFace
+//
+//  Created by Suncry on 13-10-16.
+//  Copyright (c) 2013年 ipointek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyBigFaceAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyBigFace/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyBigFace
+//
+//  Created by Suncry on 13-10-16.
+//  Copyright (c) 2013年 ipointek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MyBigFaceAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MyBigFaceAppDelegate class]));
+    }
+}
